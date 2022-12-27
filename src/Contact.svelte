a86d24cec17cf37ab27a7a3c98c05ae829545dbc
Efrainq07 <efrainq07@hotmail.com>
2022-12-27 13:26:41 -0800

    [CSS] Solved minor CSS alignment on Contact page.

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,10 +1,10 @@
 <script>
   import SocialIcons from "@rodneylab/svelte-social-icons";
+  import NavbarFiller from "./NavbarFiller.svelte";
 </script>
 
 <main>
   <div id="info-container">
-    <div class="image-cropper" />
     <div id="description">
       <h3>
         You can contact me if you have any ideas, proposals, or just want to
@@ -24,7 +24,7 @@
             <SocialIcons network="email" height="50" width="50" alt=""  fgColor="rgb(150,150,150)" bgColor="#fff" />efrainq07@gmail.com</a
           >
           <a href="https://www.lensfrens.xyz/nanquiedo.lens" rel="noreferrer"  target="_blank" class="social-link">
-            <img height="25" width="25" src="/lens-grey.png" alt="Lens Logo" /><span style="padding-left: 1vw;">nanquiedo.lens</span></a
+            <img height="25" width="25" class="lens-logo" src="/lens-grey.png" alt="Lens Logo" /><span>nanquiedo.lens</span></a
           >
       </div>
     </div>
@@ -32,6 +32,9 @@
 </main>
 
 <style>
+  .lens-logo{
+    padding: 12.5px;
+  }
 .social-link{
     display: flex;
     align-items: center;
@@ -59,15 +62,6 @@
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-  }
-  .image-cropper {
-    height: 10vw;
-    width: 10vw;
-    min-width: 300px;
-    min-height: 300px;
-    border-radius: 50%;
-    background: url("../aroba.png") 50% 5% no-repeat;
-    background-size: 100%;
   }
   #description {
     margin: 5vw;
